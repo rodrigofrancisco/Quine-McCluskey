d35a3036c83b2d5c552ddcbf3fe7bb920a04e05f
RodrigoFrancisco <rodrigofranciscopablo@gmail.com>
2019-04-18 16:38:07 -0500

    Estructura del proyecto con clases

--- js/init.js
+++ js/init.js
@@ -3,14 +3,23 @@ function init() {
   const input_dontcare = document.getElementById('dontcare').value;
 
   /*FALTA validar que los input solo contengan numeros y que esten sepa-
-    rados por comas y   que NO se repitan los elementos
+    rados por comas y   que NO se repitan los elementos y que No esten vacias
   */
-  
-  /*Los inputs los recibimos como string pero los guardamos como arrays y para ello separamos cada elemento y lo guardmos como number*/
-  let minterm = input_minterm.split(",");
-  let dontcare = input_dontcare.split(",");
 
-  /*Iniciamos el Quine-McCluskey con un arreglo que contiene los minterminos y los dont care juntos.*/
-  quineMcCluskey(minterm.concat(dontcare));
+  /*Los inputs los recibimos como string pero los guardamos como arrays y para ello separamos cada elemento y lo guardmos como number*/
+  let minterm = []
+  let dontcare = []
+
+  if (input_minterm.length != 0 )
+    minterm = input_minterm.split(",").map(Number);
+
+  if (input_dontcare.length != 0)
+    dontcare = input_dontcare.split(",").map(Number);
+
+  /*Iniciamos el Quine-McCluskey con un arreglo que contiene los minterminos y
+    los dont care juntos ya que el método sabe que hacer */
+  if(minterm.length > 0)
+    quineMcCluskey(minterm.concat(dontcare));
+  else alert("Debe haber por lo menos 1 término");
 
 }
--- js/qnmc.js
+++ js/qnmc.js
@@ -1,4 +1,20 @@
+class Termino {
+  constructor(match_pair,fp,used) {
+    this.match_pair = match_pair;
+    this.fp = fp;
+    this.used = used;
+  }
+
+  addMatchPair(item){
+    this.match_pair.push(item);
+  }
+  addFp(item){
+    this.fp.push(item);
+  }
+}
+
 function quineMcCluskey(terminos) {
-  alert(terminos)
-  alert(terminos[0].toString(2))
+  let t1 = new Termino([1,2],[1],false);
+  t1.addMatchPair(3);
+  alert(t1.match_pair);
 }
