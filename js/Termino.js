6a5e2ec6bc17d4a578cbf3ef14d4f80d5c83d152
RodrigoFrancisco <rodrigofranciscopablo@gmail.com>
2019-04-18 23:33:01 -0500

    Module class Termino in a separe file

---
+++ js/Termino.js
@@ -0,0 +1,23 @@
+class Termino {
+  constructor() {
+    this.match_pair = [];
+    this.fp = [];
+    this.used = false;
+  }
+
+  addMatchPair(item){
+    this.match_pair.push(item);
+  }
+  addFp(item){
+    this.fp.push(item);
+  }
+}
+
+function contarUnos(numero) {
+  var contador = 0;
+  while (numero) {
+    numero &= (numero-1);
+    contador++;
+  }
+  return contador;
+}
