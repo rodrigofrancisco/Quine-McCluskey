b635a6e2251c12aace530b96dc4979a9f4eea268
PROTECO <administrador@Mac-Mini-2522100.local>
2019-04-27 21:47:18 -0500

    Avance que guarda la it anterior

--- js/qnmc.js
+++ js/qnmc.js
@@ -1,40 +1,47 @@
 function quineMcCluskey(terminos) {
-  var terms = [] // sera un arreglo de grupos de terminos
 
-  /************* CREANDO LOS GRUPOS INICIALES *******************/
-  for (var i = 0; i < terminos.length; i++) {
-    let pos = contarUnos(terminos[i])
+  var iterations = [];
 
-    console.log("i: "+i+" pos= "+pos+"  termino:"+terminos[i]);//quitar
+  /* This are auxiliary arrays */
+  var item = [];
 
-    if(terms[pos] == null){
-      terms[pos]=[]
-      let t = new Termino()
-      t.addMatchPair(terminos[i])
-      terms[pos].push(t)
-    }else {
-      let t = new Termino()
-      t.addMatchPair(terminos[i])
-      terms[pos].push(t)
-    }
-    console.log("terms: ",terms) //quitar
+  /** Creating initial group */
+  for(var i = 0; i< terminos.length; i++){
+      let pos = contarUnos(terminos[i]);
+      let t = new Termino();
+      t.addMatchPair(terminos[i]);
+      
+      if(item[pos] == null){
+          /*This array is were our terms with 
+          n numbers of one are saved*/
+          item[pos] = [];
+          item[pos].push(t)
+      }else{
+          item[pos].push(t)
+      }
   }
+  iterations.push(item);
+  console.log(iterations)
 
-  /* Obteniendo implicantes primos */
-  var tmp_terms = []
+  /** Obtaining prime implicants */
 
-  for (var i = 0; i < terms.length-1; i++) {
-    if( terms[i] != null){
-      for (var j = 0; j < terms[i].length; j++) {
-        let diff = terms[i+1][j].match_pair[0]-terms[i][j].match_pair[0];
-        if (esPotencia2(diff)) {
-          //unir los terminos
+  /* Clear array 'cause is saved in iterations */
+  item = [];
+
+  for(var i = 0 ; i < iterations[0].length -1 ; i++){
+    if(iterations[0] !== null){
+      for( var j = 0 ; j < iterations[0][i].length ; j++ ){
+        for(var k = 0 ; k < iterations[0][i+1].length; k++ ){
+          let termino1 = iterations[0][i][j].match_pair[0];
+          let termino2 = iterations[0][i+1][k].match_pair[0];
+          let diff = termino2 - termino1
+          if(esPotencia2(diff)){
+            //joint terms
+          }  
         }
       }
     }
   }
-
-
 
 
 }
