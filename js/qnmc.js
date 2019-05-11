f8ee95c7ca9efcfa9c16140926d9c524178eb0c7
rhofp <rhodfra@gmail.com>
2019-05-11 04:13:14 -0500

    almost almost done

--- js/qnmc.js
+++ js/qnmc.js
@@ -1,7 +1,9 @@
 function quineMcCluskey(terminos) {
   /* This are auxiliary arrays */
   var item = [];
-  var buffer =[];
+  var iterations = [];
+  var it = 0;
+  var bandera = false;
 
   /** Creating initial group */
   for(var i = 0; i< terminos.length; i++){
@@ -10,43 +12,35 @@ function quineMcCluskey(terminos) {
       t.add_mp(terminos[i]);
       addTerm(t,item,pos);
   }
-  console.log(item);
+  iterations.push(item)
+  console.log(iterations[0]);
 
-  /** Obtaining prime implicants */
-  for(var i = 0 ; i < item.length-1 ; i++)
-    if(item[i] != null)
-      for( var j = 0 ; j < item[i].length ; j++ )
-        for(var k = 0 ; k < item[i+1].length; k++ )
-          if (fp_equals(item[i][j].fp,item[i+1][k].fp))
-            if (diffPotencia2(item[i][j].mp,item[i+1][k].mp)) {
-              let t = new Termino();
-              t.mp = item[i][j].mp.concat(item[i+1][k].mp)
-              /* i is the position of new term in new iteration*/
-              t.fp =item[i][j].fp.slice()
-              t.add_fp(item[i+1][k].mp[0]-item[i][j].mp[0])
-              t.mp.sort((a, b)=> a-b);
-              t.fp.sort((a, b)=> a-b);
-              addTerm(t,buffer,i);
-            }
+  while (bandera==false) {
 
-  console.log(buffer);
-  item =[]
+    item = iterations[it];
+    var buffer =[];
 
-  for(var i = 0 ; i < buffer.length-1 ; i++)
-    if(buffer[i] != null)
-      for( var j = 0 ; j < buffer[i].length ; j++ )
-        for(var k = 0 ; k < buffer[i+1].length; k++ )
-          if (fp_equals(buffer[i][j].fp,buffer[i+1][k].fp))
-            if (diffPotencia2(buffer[i][j].mp,buffer[i+1][k].mp)) {
-              let t = new Termino();
-              t.mp = buffer[i][j].mp.concat(buffer[i+1][k].mp)
-              /* i is the position of new term in new iteration*/
-              t.fp = buffer[i][j].fp.slice()
-              t.add_fp(buffer[i+1][k].mp[0]-buffer[i][j].mp[0])
-              t.mp.sort((a, b)=> a-b);
-              t.fp.sort((a, b)=> a-b);
-              addTerm(t,item,i);
-            }
-  console.log(item);
+    /** Obtaining prime implicants */
+    for(var i = 0 ; i < item.length-1 ; i++)
+      if(item[i] != null)
+        for( var j = 0 ; j < item[i].length ; j++ )
+          for(var k = 0 ; k < item[i+1].length; k++ )
+            if (fp_equals(item[i][j].fp,item[i+1][k].fp))
+              if (diffPotencia2(item[i][j].mp,item[i+1][k].mp)) {
+                let t = new Termino();
+                t.mp = item[i][j].mp.concat(item[i+1][k].mp)
+                /* i is the position of new term in new iteration*/
+                t.fp =item[i][j].fp.slice()
+                t.add_fp(item[i+1][k].mp[0]-item[i][j].mp[0])
+                t.mp.sort((a, b)=> a-b);
+                t.fp.sort((a, b)=> a-b);
+                addTerm(t,buffer,i);
+              }
+    bandera = item_equals(buffer);
+
+    iterations.push(buffer);
+    it++;
+    console.log(iterations[it]);
+  }
 
 }
