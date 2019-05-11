0d58464f0cbf38e5921571e478c08aeb4169ec49
rhofp <rhodfra@gmail.com>
2019-05-11 01:45:04 -0500

    Almost done

--- js/qnmc.js
+++ js/qnmc.js
@@ -1,6 +1,6 @@
 function addTerm(t,item,pos){
   if(item[pos] == null){
-    /*This array is were our terms with 
+    /*This array is were our terms with
     n numbers of one are saved*/
     item[pos] = [];
     item[pos].push(t)
@@ -9,80 +9,93 @@ function addTerm(t,item,pos){
   }
 }
 
-function quineMcCluskey(terminos) {
+function fp_equals(fp1,fp2) {
+  for(let i = 0 ; i < fp1.length; i++)
+    if(fp1[i] != fp2[i])
+      return false;
+  return true;
+}
 
-  var iterations = [];
+function diffPotencia2(mp1,mp2) {
+  diff = []
+  for (let i = 0; i < mp1.length; i++) {
+    diff.push(mp2[i] - mp1[i])
+    if (!esPotencia2(diff[i])) {
+      return false;
+    }
+  }
+  if(diff.every( (val, i, arr) => val === arr[0] ))
+    return true;
+  else
+    return false;
+}
+
+function quineMcCluskey(terminos) {
 
   /* This are auxiliary arrays */
   var item = [];
+  var buffer =[];
 
   /** Creating initial group */
   for(var i = 0; i< terminos.length; i++){
       let pos = contarUnos(terminos[i]);
       let t = new Termino();
-      t.addMatchPair(terminos[i]);
+      t.add_mp(terminos[i]);
       addTerm(t,item,pos);
   }
-  iterations.push(item);
-  console.log(iterations);
+  console.log(item);
 
-  /* Clear array 'cause is saved in iterations */
-  item = [];
- 
   /** Obtaining prime implicants */
-  for(var i = 0 ; i < iterations[0].length-1 ; i++){
-    if(iterations[0][i] != null){
-      for( var j = 0 ; j < iterations[0][i].length ; j++ ){
-        for(var k = 0 ; k < iterations[0][i+1].length; k++ ){
-          let termino1 = iterations[0][i][j].match_pair[0];
-          let termino2 = iterations[0][i+1][k].match_pair[0];
-          console.log("termino1: ",termino1);
-          console.log("termino2: ",termino2);
+  for(var i = 0 ; i < item.length-1 ; i++){
+    if(item[i] != null){
+      for( var j = 0 ; j < item[i].length ; j++ ){
+        for(var k = 0 ; k < item[i+1].length; k++ ){
+          let termino1 = item[i][j].mp[0];
+          let termino2 = item[i+1][k].mp[0];
           let diff = termino2 - termino1;
           if(esPotencia2(diff)){
             let t = new Termino();
-            t.addMatchPair(termino1);
-            t.addMatchPair(termino2);
-            t.addFp(Math.abs(termino2-termino1));
+            t.add_mp(termino1);
+            t.add_mp(termino2);
+            t.add_fp(Math.abs(termino2-termino1));
             /* i is the position of new term in new iteration*/
-            addTerm(t,item,i);
-          }  
+            addTerm(t,buffer,i);
+          }
         }
       }
     }
   }
-  iterations.push(item)
-  console.log(iterations[1])
+  console.log(buffer);
+  item =[]
 
-  /* Clear array 'cause is saved in iterations */
-  item = [];
-
-  for(var i = 0 ; i < iterations[1].length-1 ; i++){
-    if(iterations[1][i] != null){
-      for( var j = 0 ; j < iterations[1][i].length ; j++ ){
-        for(var k = 0 ; k < iterations[1][i+1].length; k++ ){
-          let termino1 = iterations[1][i][j].match_pair[0];
-          let termino2 = iterations[1][i][j].match_pair[1];
-          let termino3 = iterations[1][i+1][k].match_pair[0];
-          let termino4 = iterations[1][i+1][k].match_pair[1];
-          let diff1 = termino3 - termino1;
-          let diff2 = termino4 - termino2;
-          if(esPotencia2(diff1) && esPotencia2(diff2)){
-            let t = new Termino();
-            t.addMatchPair(termino1);
-            t.addMatchPair(termino2);
-            t.addMatchPair(termino3);
-            t.addMatchPair(termino4);
-            t.addFp(Math.abs(diff1));
-            t.addFp(Math.abs(diff2));
-            /* i is the position of new term in new iteration*/
-            addTerm(t,item,i);
-          }  
+  /** Obtaining prime implicants */
+  for(var i = 0 ; i < buffer.length-1 ; i++){
+    if(buffer[i] != null){
+      for( var j = 0 ; j < buffer[i].length ; j++ ){
+        for(var k = 0 ; k < buffer[i+1].length; k++ ){
+          if (fp_equals(buffer[i][j].fp,buffer[i+1][k].fp)) {
+            let termino1 = buffer[i][j].mp[0];
+            let termino2 = buffer[i][j].mp[1];
+            let termino3 = buffer[i+1][k].mp[0];
+            let termino4 = buffer[i+1][k].mp[1];
+            let diff1 = termino3-termino1
+            let diff2 = termino4-termino2
+            if (esPotencia2(diff1) && esPotencia2(diff2) && diff1==diff2) {
+              let t = new Termino();
+              t.add_mp(termino1);
+              t.add_mp(termino2);
+              t.add_mp(termino3);
+              t.add_mp(termino4);
+              t.add_fp(diff1);
+              t.add_fp(diff2);
+              /* i is the position of new term in new iteration*/
+              addTerm(t,item,i);
+            }
+          }
         }
       }
     }
   }
-  console.log(item)
-
+  console.log(item);
 
 }
