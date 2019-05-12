d36669de7a4cd5373efa2ef403a4765bb8c604bf
rhofp <rhodfra@gmail.com>
2019-05-11 20:33:40 -0500

    Beautyfying

--- js/qnmc.js
+++ js/qnmc.js
@@ -3,7 +3,8 @@ function quineMcCluskey(terminos) {
   var item = [];
   var iterations = [];
   var it = 0;
-  var bandera = false;
+  var ipe = []
+  var flag = false;
 
   /** Creating initial group */
   for(var i = 0; i< terminos.length; i++){
@@ -13,35 +14,38 @@ function quineMcCluskey(terminos) {
       addTerm(t,item,pos);
   }
   iterations.push(item)
-  console.log(iterations[0]);
+  //console.log(iterations[0]);
 
-  while (!bandera) {
+  while (!flag) {
 
     item = iterations[it];
     var buffer =[];
 
     /** Obtaining prime implicants */
-    for(var i = 0 ; i < item.length-1 ; i++)
-      if(item[i] != null)
-        for( var j = 0 ; j < item[i].length ; j++ )
-          for(var k = 0 ; k < item[i+1].length; k++ )
-            if (fp_equals(item[i][j].fp,item[i+1][k].fp))
-              if (diffPotencia2(item[i][j].mp,item[i+1][k].mp)) {
-                let t = new Termino();
-                t.mp = item[i][j].mp.concat(item[i+1][k].mp)
-                /* i is the position of new term in new iteration*/
-                t.fp =item[i][j].fp.slice()
-                t.add_fp(item[i+1][k].mp[0]-item[i][j].mp[0])
-                t.mp.sort((a, b)=> a-b);
-                t.fp.sort((a, b)=> a-b);
-                addTerm(t,buffer,i);
-              }
+    for(var i = 0 ; i < item.length-1 ; i++) if(item[i] != null)
+      for( var j = 0 ; j < item[i].length ; j++ )
+        for(var k = 0 ; k < item[i+1].length; k++ )
+          if (fp_equals(item[i][j].fp,item[i+1][k].fp)
+            && diffPotencia2(item[i][j].mp,item[i+1][k].mp)) {
+            item[i][j].used = true;
+            item[i+1][k].used = true;
+
+            let t = new Termino();
+            t.mp = item[i][j].mp.concat(item[i+1][k].mp)
+            t.fp =item[i][j].fp.slice()
+            t.add_fp(item[i+1][k].mp[0]-item[i][j].mp[0])
+            t.mp.sort((a, b)=> a-b);
+            t.fp.sort((a, b)=> a-b);
+            /* i is the position of new term in new iteration*/
+            addTerm(t,buffer,i);
+          }
     if (buffer.length > 0) {
       iterations.push(buffer);
       it++;
-      console.log(iterations[it]);
+      //console.log(iterations[it]);
     }else {
-      bandera = true;
+      flag = true;
     }
   }
+  console.log(iterations);
 }
